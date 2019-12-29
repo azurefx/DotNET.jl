632307f001bbc0f57d0bdbe8816734e70a262af6
AzureFx <i@azurefx.name>
2019-12-30 01:42:17 +0800

    Support iteration over generic enumerables

--- src/array.jl
+++ src/array.jl
@@ -6,6 +6,14 @@ function iterate_ienumerable(enumerator::CLRObject)
     return (next, (enumerator, enumeratorty))
 end
 
+function iterate_ienumerable_t(enumerator::CLRObject, elt::CLRObject)
+    enumeratorty = makegenerictype(T"System.Collections.Generic.IEnumerator`1", elt)
+    hasnext = invokemember(enumeratorty, enumerator, :MoveNext)
+    hasnext || return nothing
+    next = invokemember(enumeratorty, enumerator, :Current)
+    return (next, (enumerator, enumeratorty))
+end
+
 function Base.iterate(obj::CLRObject)
     gethandle(obj) == 0 && return nothing
     objty = clrtypeof(obj)
@@ -13,6 +21,14 @@ function Base.iterate(obj::CLRObject)
     if isassignable(enumerablety, objty)
         enumerator = invokemember(enumerablety, obj, :GetEnumerator)
         return iterate_ienumerable(enumerator)
+    end
+    elt = clreltype(obj)
+    if gethandle(elt) != 0
+        enumerablety = makegenerictype(T"System.Collections.Generic.IEnumerable`1", elt)
+        if isassignable(enumerablety, objty)
+            enumerator = invokemember(enumerablety, obj, :GetEnumerator)
+            return iterate_ienumerable(enumerator)
+        end
     end
     throw(ArgumentError("Object is not iterable"))
 end
@@ -43,6 +59,8 @@ function Base.length(obj::CLRObject)
     objty = clrtypeof(obj)
     if isassignable(T"System.Array", objty)
         invokemember(T"System.Array", obj, :Length)
+    elseif isassignable(T"System.Collections.IList", objty)
+        invokemember(obj, :Count)
     else
         throw(ArgumentError("Cannot determine length from type $objty"))
     end
